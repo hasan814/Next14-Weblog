bd7d7ec08834172bdd2faeeeb55ad2cc895a3a1b
hasan814 <h.mousavi910@gmail.com>
2024-11-06 21:34:16 +0330

    Add functions to retrieve data for posts and users
    - Implement getDatas to fetch all posts from the database
      - Includes error handling for potential fetch issues
    - Implement getData to retrieve a single post by ID
      - Adds error handling for cases where the post is not found
    - Implement getUser to fetch a user by ID from the database
      - Ensures graceful handling of errors in case of user retrieval failure

---
+++ src/utils/FetchDataDB.ts
@@ -0,0 +1,61 @@
+import { connectDB } from "@/lib/connectDB";
+import Post from "@/model/PostModel";
+import User from "@/model/UserModel";
+
+
+// =========== Fetch all posts ============
+export const getDatas = async () => {
+    try {
+        connectDB()
+        const posts = await Post.find();
+        return posts;
+    } catch (error) {
+        console.error("Error fetching posts:", error);
+        throw new Error("Could not fetch posts");
+    }
+};
+
+// ============= Fetch a single post by ID =========
+export const getData = async (slug: string) => {
+    try {
+        connectDB()
+        const post = await Post.find({ slug });
+        if (!post) {
+            throw new Error("Post not found");
+        }
+        return post;
+    } catch (error) {
+        console.error("Error fetching post:", error);
+        throw new Error("Could not fetch post");
+    }
+};
+
+// ========== Fetch a single user by ID ============
+export const getUser = async (userId: string) => {
+    try {
+        connectDB()
+        const user = await User.findById(userId);
+        if (!user) {
+            throw new Error("User not found");
+        }
+        return user;
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        throw new Error("Could not fetch user");
+    }
+};
+
+// ========== Fetch all Users ============
+export const getUsers = async () => {
+    try {
+        connectDB()
+        const users = await User.find();
+        if (!users) {
+            throw new Error("Users not found");
+        }
+        return users;
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        throw new Error("Could not fetch user");
+    }
+};
